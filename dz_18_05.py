d98da7a799d05da06cf049f7c0fb9a00ffae7e89
Your Name <shapettt@gmail/com>
2020-05-17 17:01:25 +0300

    first pars2

--- dz_18_05.py
+++ dz_18_05.py
@@ -5,7 +5,6 @@ with open('pars.txt', 'r') as f:
     for text in f:
         if 'Mozilla' in text :
             firefox += 1
-firefox = str(firefox)
 print('Mozilla = ', firefox)
 
 google = 0
@@ -13,7 +12,6 @@ with open('pars.txt', 'r') as f:
     for text in f:
         if 'Chrome' in text :
             google += 1
-google = str(google)
 print('Chrome = ', google)
 
 saf = 0
@@ -21,5 +19,4 @@ with open('pars.txt', 'r') as f:
     for text in f:
         if 'Safari' in text :
             saf += 1
-saf = str(saf)
 print('Safari = ', saf)
